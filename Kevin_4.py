74651356f7e2efecf479711c6fa73097e8bde8bf
Chohongjae <kevin@wivlabs.com>
2018-03-19 17:53:28 +0900

    0.620

--- Kevin_4.py
+++ Kevin_4.py
@@ -7,5 +7,5 @@ for i in range(100,1000):
         if str(i*j)[0:3] == str(i*j)[:2:-1]: #3자리수 * 3자리수를 문자열로 바꾼뒤 앞뒤로 뒤집어서 자른뒤 같은지 비교
             if maxValue < (i*j): #초기값보다 크면 maxValue값 교체
                 maxValue = i*j   #다음부터 생성되는 값을 그 전 maxValue와 비교
-print(maxValue)
+print(maxValue) 
 
