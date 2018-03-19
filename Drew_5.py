e1b3ea33224e5665dd4250b31ffe9d4fd9908e64
drew <drew@wivlabs.com>
2018-03-19 16:32:59 +0900

    35.97096037864685

--- Drew_5.py
+++ Drew_5.py
@@ -1,3 +1,6 @@
+import time
+
+start = time.time()
 # Q.5
 # 1 ~ 10 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 2520입니다.
 
@@ -5,5 +8,21 @@
 
 # Sol.5
 
+# a =1
+# for j in range(1,21):
+#     a *= j
+#     print(a)
+# 1*2*3*...*20=2432902008176640000
+
+a=0
+for i in range(1,2432902008176640000):
+    if i % 20 == 0 and i % 19 == 0 and i % 18 == 0 and i % 17 == 0 and i % 16 == 0 and i % 15== 0 and i % 14== 0 and i % 13== 0 and i % 12== 0 and i % 11== 0 :
+        print(i)
+        break
+
+
+
 # A.5
-232792560
+# 232792560
+end = time.time() - start
+print(end)
