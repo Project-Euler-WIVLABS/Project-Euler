be8968e3c1d49db4acd233807311cfb389e67a1a
Chohongjae <kevin@wivlabs.com>
2018-03-20 10:10:08 +0900

    0.001

--- Kevin_1.py
+++ Kevin_1.py
@@ -1,6 +1,7 @@
+list = [i for i in range(1000) if i % 3 == 0 or i % 5 == 0]
 sum = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        sum +=i
+for i in list:
+    sum +=i
 print(sum)
+
 
