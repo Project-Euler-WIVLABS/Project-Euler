9bec38cf9f5838706fba6fc97a5e4b22f82ce4c8
Chohongjae <kevin@wivlabs.com>
2018-03-21 15:17:21 +0900

    0.532

--- Kevin_4.py
+++ Kevin_4.py
@@ -1,11 +1,13 @@
-a = [] #최대값을 담기위한 리스트
-maxValue = 1000 #초기 최대값은 1000
-for i in range(100,1000):
-    for j in range(100,1000):
-        if i * j < 100000 :
-            continue
-        if str(i*j)[0:3] == str(i*j)[:2:-1]: #3자리수 * 3자리수를 문자열로 바꾼뒤 앞뒤로 뒤집어서 자른뒤 같은지 비교
-            if maxValue < (i*j): #초기값보다 크면 maxValue값 교체
-                maxValue = i*j   #다음부터 생성되는 값을 그 전 maxValue와 비교
-print(maxValue) 
+def palindrome():
+    maxValue = 1000  # 초기 최대값은 1000
+    for i in range(999,100,-1):
+        for j in range(999,100,-1):
+            if i * j < 100000 :
+                break
+            if str(i*j)[0:3] == str(i*j)[:2:-1]:
+                if maxValue < i*j:#3자리수 * 3자리수를 문자열로 바꾼뒤 앞뒤로 뒤집어서 자른뒤 같은지 비교
+                    maxValue = i*j
+    return maxValue#다음부터 생성되는 값을 그 전 maxValue와 비교
+
+print(palindrome())
 
