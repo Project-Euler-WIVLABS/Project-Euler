23ed9d100985e3f7310dde1829e6b84823b8a469
Chohongjae <kevin@wivlabs.com>
2018-04-04 15:14:58 +0900

    26.20

---
+++ Kevin_27.py
@@ -0,0 +1,40 @@
+import math
+def abc(i,j):
+    for n in range(100):
+        if ab((n * n) + (i * n) + j):
+            print((n * n) + (i * n) + j)
+            continue
+        else:
+            return n
+
+def ab(k):
+    isPrime = False
+    if k > 0:
+        for j in range(2, int(math.sqrt(k)) + 1):
+            if k % j != 0:
+                isPrime = True
+            else:
+                return False
+        if isPrime == True:
+            return True
+    else:
+        for j in range(-2,k,-1):
+            if k % j != 0:
+                isPrime = True
+            else:
+                return False
+        if isPrime == True:
+            return True
+
+maxprime = 0
+a = []
+b = 0
+c = 0
+for i in range(-999,1000):
+    for j in range(-999,1000):
+        if abc(i,j) > maxprime:
+            maxprime = abc(i,j)
+            b = i
+            c = j
+
+print(b,c)
